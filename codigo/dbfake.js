46dd2ad75cb56f4448dfad143e183099cfdb9b3e
luckaris <57960595+luckaris@users.noreply.github.com>
2022-05-11 14:34:00 -0300

    implementação DB estatica

---
+++ codigo/dbfake.js
@@ -0,0 +1,35 @@
+var db = {
+    dados: [
+        {
+            tipo: 'Cachorro',
+            cor: 'vermelho',
+            descricao: 'coleira preta e olhos pretos',
+            imagem: 'https://placekitten.com/320/420/?1',
+            porte: 'pequeno',
+        }, {
+            tipo: 'Gato',
+            cor: 'branco',
+            descricao: 'olhos vermelhos',
+            imagem: 'https://placekitten.com/320/420/?2',
+            porte: 'médio',
+        }, {
+            tipo: 'Cachorro',
+            cor: 'caramelo',
+            descricao: 'pelagem comprida',
+            imagem: 'https://placekitten.com/320/420/?1',
+            porte: 'médio',
+        }, {
+            tipo: 'Cachorro',
+            cor: 'branco',
+            descricao: 'pintas pretas',
+            imagem: 'https://placekitten.com/320/420/?4',
+            porte: 'grande',
+        }, {
+            tipo: 'Gato',
+            cor: 'preto',
+            descricao: 'olhos azuis e barriga branca',
+            imagem: 'https://placekitten.com/320/420/?5',
+            porte: 'pequeno',
+        }
+    ]
+}
